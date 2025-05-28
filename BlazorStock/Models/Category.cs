430b9a73a7f7f360e72c90ec88d54989dccfc110
DedeDoidao <deniscostaf@gmail.com>
2025-05-27 21:11:43 -0300

    Começando a página de Create

--- BlazorStock/Models/Category.cs
+++ BlazorStock/Models/Category.cs
@@ -12,10 +12,10 @@ namespace BlazorStock.Models
         [Required(ErrorMessage = "Id é obrigatório")]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Titulo é obrigatório")]
-        [MaxLength(50, ErrorMessage = "O título deve ter no máximo 50 caracteres")]
-        [MinLength(5, ErrorMessage = "O título deve ter no mínimo 5 caracteres")]
-        public string Title { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O Nome deve ter no máximo 50 caracteres")]
+        [MinLength(5, ErrorMessage = "O Nome deve ter no mínimo 5 caracteres")]
+        public string Name { get; set; } = string.Empty;
 
         public List<Product> Products { get; set; } = new List<Product>();
     }
